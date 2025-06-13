0ef93b577986c7bd675f58f303cfb26735a69dc5
Lilirtu <leacharafeddine@icloud.com>
2025-06-13 18:54:44 +0200

    creation of the User, Role, Feature, Room, RoomFeature table

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE Role(
     Description TEXT NOT NULL
 );
 
-Create TABLE Users (
+CREATE TABLE Users (
     Id INT PRIMARY KEY AUTO_INCREMENT,
     FullName VARCHAR(100) NOT NULL,
     Email VARCHAR(200) NOT NULL UNIQUE,
@@ -14,3 +14,24 @@ Create TABLE Users (
         ON DELETE RESTRICT -- prevents deleting a role if users still use it
         ON UPDATE CASCADE -- keeps foreign keys updated if the role's ID changes
 ); 
+
+CREATE TABLE Room(
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(100) NOT NULL,
+    Location VARCHAR(100) NOT NULL,
+    Capacity TINYINT UNSIGNED NOT NULL
+);
+
+CREATE TABLE Feature(
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    FeatureName VARCHAR(100) NOT NULL,
+    Description VARCHAR(250) NOT NULL 
+);
+
+CREATE TABLE RoomFeature(
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    RoomId INT NOT NULL,
+    FeatureId INT NOT NULL,
+    FOREIGN KEY (RoomId) REFERENCES Room(Id),
+    FOREIGN KEY (FeatureId) REFERENCES Feature(Id)
+);
