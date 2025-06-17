22dc9802628d95a3a33d8d9730ef7c861baaa366
Lilirtu <leacharafeddine@icloud.com>
2025-06-17 19:56:10 +0200

    modified Id in the methods to id to avoid errors

--- backend/Laravel1/app/Models/Role.php
+++ backend/Laravel1/app/Models/Role.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model; // it make the class to be able to talk 
 class Role extends Model { 
 
     protected $table = 'Role'; // let laravel know in which table we work to avoid confusion because laravel take the default
-    protected $primaryKey = 'Id'; // same idea by default laravel take th name of the primary key id not Id so to avoid errors and not found columns
+    protected $primaryKey = 'id'; // same idea by default laravel take th name of the primary key id not Id so to avoid errors and not found columns
     public $timestamps = false; // Laravel automatically expects and manages two timestamp columns: created_at and updated_at. If your table does not have these columns, you set this to false to prevent errors.
     protected $fillable = ['RoleName', 'Description']; // to let laravel know which columns can be filled because by defaults it blocks the assignment on all field
     
--- backend/Laravel1/app/Models/Users.php
+++ backend/Laravel1/app/Models/Users.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model; // it make the class to be able to talk 
 class Users extends Model { 
 
     protected $table = 'Users'; // let laravel know in which table we work to avoid confusion because laravel take the default exemple users for User and here we use Users so to not make confusion
-    protected $primaryKey = 'Id'; // same idea by default laravel take th ename of the primary key id not Id so to avoid errors and not found columns
+    protected $primaryKey = 'id'; // same idea by default laravel take th ename of the primary key id not Id so to avoid errors and not found columns
     public $timestamps = false; // Laravel automatically expects and manages two timestamp columns: created_at and updated_at. If your table does not have these columns, you set this to false to prevent errors.
     protected $fillable = ['FullName', 'Email', 'Password', 'RoleId']; // to let laravel know which columns can be filled because by defaults it blocks the assignment on all field
     protected $hidden = ['Password']; // to hide some sensitive information and not share it raw 
