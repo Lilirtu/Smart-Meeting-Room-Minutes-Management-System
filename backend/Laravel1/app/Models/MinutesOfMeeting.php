0aa395288a53e8191544dbbfc6a483ba0059569d
RamaAlassi <rama.alassi@outlook.com>
2025-06-20 21:27:35 +0300

    Update MinutesOfMeeting.php

---
+++ backend/Laravel1/app/Models/MinutesOfMeeting.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MinutesOfMeeting extends Model
+{
+    protected $table = 'MinutesOfMeeting';
+    public $timestamps = false;
+    protected $primaryKey = 'Id';
+    protected $fillable = ['Topic', 'Summary', 'DecisionMade'];
+}
